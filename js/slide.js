ba06a251430ce380ea57daab71f67567e8ccdaa9
eliabearaujo <eliabe_trajano@hotmail.com>
2022-10-27 01:49:13 -0300

    Eventos de touch adicionados

--- js/slide.js
+++ js/slide.js
@@ -34,27 +34,45 @@ export default class Slide {
   // 1 - Criação do método que irá prevenir o "arraste de imagem" e
   // que ativa a função de mousemove, após um clique no wrapper.
   onStart(event) {
-    event.preventDefault();
+    // Recebe o tipo de movimento que está sendo feito
+    let movetype;
+    // Verifica o tipo de evento, para tomar diferentes ações
+    if (event.type === 'mousedown') {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      movetype = 'mousemove';
+    } else {
+      // Passa para o dist o ponto de clique no eixo x
+      this.dist.startX = event.changedTouches[0].clientX;
+      movetype = 'touchmove';
+    }
     // Adicionamos esse evento aqui, para ele apenas seja executado após o primeiro click
     // Assim a função onMove será executada sempre que movermos o mouse
     // Mas apenas após clicar no wrapper.
-    this.wrapper.addEventListener('mousemove', this.onMove);
-    // Passa para o dist o ponto de clique no eixo x
-    this.dist.startX = event.clientX;
+    // Fica fora do ifelse pois recebe o parametro do tipo de movimento desse check.
+    // Assim não precisamos adionar a linha abaixo no ifelse
+    this.wrapper.addEventListener(movetype, this.onMove);
   }
 
   // 4 - Dados do evento de movimentação do mouse enquanto clicado.
   onMove(event) {
+    // Para passar o valor correto e tipo de evento para o updatePosition
+    // precisamos pegar a posição através do client X ou changedTouches
+    const pointerPosition =
+      event.type === 'mousemove'
+        ? event.clientX
+        : event.changedTouches[0].clientX;
     // Armazena o valor do retorno da chamada do método updatePosition.
-    const finalPosition = this.updatePosition(event.clientX);
+    const finalPosition = this.updatePosition(pointerPosition);
     // Move o slide para a posição atualizada.
     this.moveSlide(finalPosition);
-    console.log(this.dist);
   }
 
   // 5 - Remove o evento de mousemove ao "desclicar"
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+    // Vericamos o tipo de movimento para armazenar a ultima coordenada correta.
+    const movetype = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
+    this.wrapper.removeEventListener(movetype, this.onMove);
     // Informarmos para o finalPosition a ultima posição no eixo x do mouse quando tiramos ele de cima.
     // E atualizamos no objeto.
     this.dist.finalPosition = this.dist.movePosition;
@@ -65,8 +83,12 @@ export default class Slide {
     // Adiciona o evento de clique e arraste sobre as imagens
     // E executa a função onStart, que previne que a imagem seja "arrastada"
     this.wrapper.addEventListener('mousedown', this.onStart);
+    // Evento similar ao mousedown porém para mobile
+    this.wrapper.addEventListener('touchstart', this.onStart);
     // Adiciona o evento de "desclique" ao wrapper e executa a função onEnd.
     this.wrapper.addEventListener('mouseup', this.onEnd);
+    // Evento similar ao mouseup, porém para mobile
+    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
   // 3 - Modifica e adiciona ao constructor o bind dos métodos.
